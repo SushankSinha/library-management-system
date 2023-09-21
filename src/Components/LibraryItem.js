37d846d4f9d343090e48917033dc133b6d5801d7
SushankSinha <SushankSinha>
2023-09-22 00:35:14 +0530

    UI update

--- src/Components/LibraryItem.js
+++ src/Components/LibraryItem.js
@@ -5,18 +5,20 @@ import Card from '@mui/material/Card';
 
 function LibraryItem({ book }) {
   return (
-    <Card className="library-item" elevation={3}>
+    <Card style={{width : '250px', height : '450px', margin : '15px', padding : '5px'}} className="library-item" elevation={3}>
       <CardContent>
-        <Typography variant="h5" component="div">
+        <Typography style = {{fontWeight : 'bold', margin : '10px auto'}} variant="h5" component="div">
           {book.title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          <strong>Author:</strong> {book.author}
+        <Typography style={{width : '200px', height : '300px', margin : '10px auto'}} variant="body2" color="text.secondary">
+           {book.poster}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          <strong>Year:</strong> {book.year}
+        <Typography style={{margin : '10px auto'}} variant="body2" color="text.secondary">
+          <strong>Rating:</strong> {book.rating}
         </Typography>
-        {/* Add more book details as needed */}
+        <Typography style={{margin : '10px auto'}} variant="body2" color="text.secondary">
+          <strong>Summary:</strong> {book.summary}
+        </Typography>
       </CardContent>
     </Card>
   );
