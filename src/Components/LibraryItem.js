cac74298e2b6514b6c4cbe33e98dfbbaf8f855ca
SushankSinha <SushankSinha>
2023-09-22 03:16:34 +0530

    UI update

--- src/Components/LibraryItem.js
+++ src/Components/LibraryItem.js
@@ -7,11 +7,14 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import axios from 'axios';
 import API_BASE_URL from './global';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import CardActions from '@mui/material/CardActions';
 
-function LibraryItem({ book }) {
+function BookCard({ book }) {
 
- const deleteBook = async() => {
+  const [available, setAvailable] = useState(true);
+
+  const deleteBook = async() => {
     try{
         await axios.delete(`${API_BASE_URL}/${book.id}`);
     }catch(err){
@@ -20,7 +23,8 @@ function LibraryItem({ book }) {
   
 };
 
-  const [available, setAvailable] = useState(true)
+const navigate = useNavigate();
+
   return (
     <Card style={{width : '350px', minHeight : '450px', height : 'fit-content', margin : '15px auto', padding : '5px'}} className="library-item" elevation={3}>
       <CardContent>
@@ -37,17 +41,13 @@ function LibraryItem({ book }) {
         <Button variant='contained' onClick={()=> setAvailable(!available)} >
          Status : {available===true? 'Available' : 'Unavailable'}
         </Button>
-        <Link to = {`/edit/${book.id}`} style={{color : 'white', textDecoration : 'none'}}>
-        <button className='btn btn-warning' style={{display : 'flex', flexDirection : 'row', margin : '5px 25px'}}>
-         <EditIcon/>
-        </button>
-        </Link>
-        <button className='btn btn-danger' onClick = {deleteBook} style={{display : 'flex', flexDirection : 'row', margin : '5px 25px'}}>
-         <DeleteIcon/>
-        </button>
       </CardContent>
+      <CardActions>
+        <Button onClick={navigate(`/edit/${book.id}`)} color='warning' size="small"><EditIcon/></Button>
+        <Button color='danger' onClick={deleteBook} size="small"><DeleteIcon/></Button>
+      </CardActions>
     </Card>
   );
 }
 
-export default LibraryItem;
+export default BookCard;
